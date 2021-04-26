45382efae6029ba93f2a6463a72da92388a2a56c
kimyujin0 <82855751+kimyujin0@users.noreply.github.com>
2021-04-26 17:48:10 +0900

    Add files via upload

---
+++ 5장 단원문제.R
@@ -0,0 +1,50 @@
+mpg <- as.data.frame(ggplot2::mpg)
+
+# 1. 자동차 배기량에 따라 고속도로 연비가 다른지 알아보려고 합니다. displ(배기량)이 4 이하인 자동차와 5 이상인 자동차 중 어떤 자동차의hwy(고속도로 연비)가 평균적으로 더 높은지 알아보세요.
+dis5 <- mpg %>% filter(displ >=5) 
+dis4 <- mpg %>% filter(displ <=4)
+mean(dis5$hwy)
+mean(dis4$hwy)  # 4이하가 더 높음 ok
+
+#2 ok
+mpg %>%filter(manufacturer %in% c('audi','toyota')) %>%
+    group_by(manufacturer) %>%
+    summarise(avg_cty=mean(cty)) %>% arrange(desc(avg_cty))        # 도요타의 평균 연비가 높다.
+
+# 3 ok
+total_avg <- mpg %>% filter(manufacturer %in% c("chevrolet", "ford", "honda"))
+mean(total_avg$hwy)
+
+# 4 ok 
+mpg %>% select(class, cty) %>% head(10)  
+
+# 5 ok
+mpg %>% filter(class %in% c('suv','compact')) %>% group_by(class) %>% summarise(avg_cty=mean(cty)) #compact
+ 
+# 6 ok
+mpg %>% filter(manufacturer=='audi') %>%group_by(manufacturer) %>% summarise(hwy) %>%arrange(desc(hwy)) %>% head(5)
+
+# 7
+#1) 합산 연비 변수 ok
+mpg$total <- (mpg$cty + mpg$hwy)
+#2) 평균 연비 변수 ok
+mpg$total_avg <- (mpg$cty + mpg$hwy)/2
+#3) 평균 연비 변수가 가장 높은 자동차 3종의 데이터를 출력하세요. ok
+mpg %>% group_by(manufacturer) %>% summarise(total_avg) %>% arrange(desc(total_avg)) %>% head()
+#4)
+a <- mpg %>% select(class, cty)
+head(a)
+    
+# 8 class별 cty 평균 ok
+mpg %>% select(class, cty) %>% group_by(class) %>% summarise(c_avg=mean(cty))
+
+# 9 ok
+mpg %>% select(class, cty) %>% group_by(class) %>% summarise(c_avg=mean(cty)) %>% arrange(desc(c_avg))%>% head(7)
+
+# 10 hwy 평균이 가장 높은 회사 세 곳을 출력하세요. ok
+mpg %>% group_by(manufacturer) %>% summarise(h_avg=mean(hwy)) %>% arrange(desc(h_avg)) %>% head(3)
+
+# 11 "compact"(경차) 차종을 가장 많이 생산하는지 알아보려고 합니다. 각 회사별 "compact" 차종 수를 내림차순으로 정렬해 출력하세요. ok
+mpg %>% filter(class=="compact") %>% group_by(manufacturer) %>% summarise(class_n=n()) %>% arrange(desc(class_n))
+
+
